7b799fb086fad5a6abe2b1f641114306563eab52
KlaKali1 <klaudia.m.kalinowska@gmail.com>
2018-09-30 15:38:08 +0200

    css changed

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 var reverseText = document.getElementById('textReversing');
 var reverseWord = document.getElementById('wordsReversing');
 var telegramText = document.getElementById('telegram');
-var capitalizeText = document.getElementById('capitalizing');
     
 // Reverse function
 function reverseTextFunction() {
@@ -13,15 +12,6 @@ if (userTextMessage == "") {
 } else { document.getElementById("toReplace").textContent = reversedTextMessage; }
 };
 
-/* // Letter capitalizing function
-function myFunctionTwo() {
-var userTextMessage = document.getElementById("message").value;
-let caitalizedMessage = userTextMessage.toUpperCase();
-    if (userTextMessage == "") {
-    alert("Please write the message");
-}
-    document.getElementById("toReplace").textContent = caitalizedMessage;
-} */
 // Reverse each word function
 function reverseWordsFunction() {
 var userTextMessage = document.getElementById("message").value;
@@ -41,19 +31,6 @@ if (userTextMessage == "") {
     alert("Please write the message");
 } else { document.getElementById("toReplace").textContent = telegramTextMessage + "."; }
 };
-
-// Capitalize each first letter function
-function capitalizeFunction() {
-var userTextMessage = document.getElementById("message").value;
-let capitalizeTextMessage = userTextMessage.charAt(0).toUpperCase();
-    
-if (userTextMessage == "") {
-    alert("Please write the message");
-} else { document.getElementById("toReplace").textContent = userTextMessage.replace(/\w\S*/g, function() {
-        return userTextMessage.charAt(0).toUpperCase() + userTextMessage.substr(1).toLowerCase();
-    }); }
-};
-
 
 reverseText.addEventListener('click', reverseTextFunction);
 reverseWord.addEventListener('click', reverseWordsFunction);
